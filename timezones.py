d6e9d7e56723f4e8a169db09d9f5e09cb9356093
America779 <126671528+RaresMihai24@users.noreply.github.com>
2024-06-06 00:28:09 +0200

    Create timezones.py

---
+++ timezones.py
@@ -0,0 +1,7 @@
+timezones = [
+    'Pacific/Midway', 'America/Adak', 'America/Anchorage', 'America/Los_Angeles',
+    'America/Denver', 'America/Chicago', 'America/New_York', 'America/Sao_Paulo',
+    'Atlantic/Azores', 'Europe/London', 'Europe/Berlin', 'Europe/Moscow',
+    'Asia/Dubai', 'Asia/Karachi', 'Asia/Kolkata', 'Asia/Bangkok', 'Asia/Tokyo',
+    'Australia/Sydney', 'Pacific/Auckland', 'Pacific/Fiji'
+]
